539c92a8e68d1a78665941b259cca48b72eba9c2
UniuyoConnect <uniuyoconnect01@gmail.com>
2026-07-19 14:55:38 +0100

    Delete layout.js

--- layout.js
+++
@@ -1,4 +0,0 @@
-export const metadata = { title: 'Uniuyo Connect', description: 'Campus social app' }
-export default function RootLayout({children}) {
-  return (<html lang="en"><body style={{margin:0}}>{children}</body></html>)
-    }
